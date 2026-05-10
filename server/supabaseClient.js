1404b87c84ed3aa41615103d6040aa34b24f5bf4
Hariharan D <hariharand2888@gmail.com>
2026-05-10 09:05:12 +0530

    fixed Variable

--- server/supabaseClient.js
+++ server/supabaseClient.js
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv'
 
-dotenv.config()
+// Vite automatically loads variables, so you don't need 'dotenv'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
